14e2b74e739442e107aa4a0fc8f3060a615b53e6
wsl on bacon <konayuki0455@gmail.com>
2018-12-04 15:30:06 +0900

    add kadai3.c

---
+++ task3/kadai3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+static double up = 3.14;
+static double down = 0;
+
+static double dx = 0.01;
+
+double sin2(double x){
+    return (sin2(x))^2;
+}
+int main(void){
+    double ans;
+    for(double x = 0; x <= up; x += dx){
+        ans += sin2(x) * dx;
+    }
+    printf("answer: %lf", ans);
+}
